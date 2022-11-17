274057efff2faeac4817c50c364a99eaadc458f7
Fieria <oleynikpolinap@yandex.ru>
2022-11-17 23:47:54 +0300

    ok

---
+++ demo/src/main/java/com/example/demo/Contact.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+public class Contact {
+    String name;
+    int number;
+    String email;
+
+    public Contact(String name, int number){
+        this.name = name;
+        this.number = number;
+    }
+
+    public Contact(String name, int number, String email){
+        this.name = name;
+        this.number = number;
+        this.email = email;
+    }
+
+    public Contact(String s){
+        String[] s2 = s.split(" ");
+        name = s2[0];
+        number = Integer.parseInt(s2[1]);
+        this.number = number;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Контакт " + name + ", номер: " + number + ", email: " + email;
+    }
+
+}
---
+++ demo/src/main/java/com/example/demo/ContactController.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@RestController
+public class ContactController {
+    private final List<Contact> contacts = new ArrayList<>();
+
+    //curl -X POST -H "Content-Type: application/json" -d '{"name":"Polina","age":16}' http://localhost:8080/contacts
+    @PostMapping("contacts")
+    public ResponseEntity<Void> addContact(@RequestBody Contact contact) {
+        contacts.add(contact);
+        return ResponseEntity.accepted().build();
+    }
+
+    //curl -X DELETE http://localhost:8080/contacts/{index}
+    @DeleteMapping("contacts/{index}")
+    public ResponseEntity<Void> deleteContact(@PathVariable("index") Integer index) {
+        contacts.remove((int) index);
+        return ResponseEntity.noContent().build();
+    }
+
+    //curl http://localhost:8080/contacts
+    @GetMapping("users")
+    public ResponseEntity<List<Contact>> getAllContacts() {
+        return ResponseEntity.ok(contacts);
+    }
+
+    //curl http://localhost:8080/contacts/{index}
+    @GetMapping("contacts/{index}")
+    public ResponseEntity<Contact> getContact(@PathVariable("index") Integer index) {
+        return ResponseEntity.ok(contacts.get(index));
+    }
+
+    //curl -X PUT -H "Content-Type: application/json" -d {contact} http://localhost:8080/contacts/{index}
+    @PutMapping("messages/{index}")
+    public ResponseEntity<Void> updateContact(
+            @PathVariable("index") Integer i,
+            @RequestBody Contact contact) {
+        contacts.remove((int) i);
+        contacts.add(i, contact);
+        return ResponseEntity.accepted().build();
+    }
+
+}
